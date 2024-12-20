568dcee9b1734db78033605f8e4108f2d998f1e4
najmiiqbalhanif <najmimahasiswa@gmail.com>
2024-12-20 21:15:26 +0700

    U_homepage

--- web/web/src/main/java/com/athletezone/web/controller/U_homepageController.java
+++ web/web/src/main/java/com/athletezone/web/controller/U_homepageController.java
@@ -1,13 +1,20 @@
 package com.athletezone.web.controller;
 
+import com.athletezone.web.dto.ProductDTO;
+import com.athletezone.web.services.ProductService;
 import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class U_homepageController {
+    private final ProductService productService;
+    public U_homepageController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/U_homepage")
     public String homepage(HttpSession session, Model model) {
@@ -19,6 +26,12 @@ public class U_homepageController {
 
             // Kirim id user ke view
             model.addAttribute("userId", userId);
+
+            // Ambil semua produk dan batasi hanya 3 produk
+            List<ProductDTO> products = productService.getAllProducts();
+            List<ProductDTO> limitedProducts = products.stream().limit(3).toList();
+
+            model.addAttribute("limitedProducts", limitedProducts); // Kirim data ke view
             return "U_homepage"; // Tampilkan halaman
         }
         return "redirect:/login"; // Redirect ke login jika session tidak valid
